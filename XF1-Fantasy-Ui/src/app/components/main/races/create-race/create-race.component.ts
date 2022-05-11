b8e32e63925f2ae9b4ad07b5b18768065a2744cf
FALCH2000 <faleiva.ch@gmail.com>
2022-05-11 12:30:44 -0600

    get info of inputs in Ui

--- XF1-Fantasy-Ui/src/app/components/main/races/create-race/create-race.component.ts
+++ XF1-Fantasy-Ui/src/app/components/main/races/create-race/create-race.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RacesService} from 'src/app/services/races.service';
 
 interface Datos {
   value: string;
@@ -15,31 +16,53 @@ export class CreateRaceComponent implements OnInit {
 
   formRaces: FormGroup;
 
-  paises: Datos[] = [
+  countries: Datos[] = [
     {value: 'Costa Rica-0', viewValue: 'Costa Rica'},
     {value: 'Panama-1', viewValue: 'Panama'},
     {value: 'Colombia-2', viewValue: 'Colombia'},
   ];
 
-  campeonatos: Datos[] = [
-    {value: 'Costa Rica-0', viewValue: 'Costa Rica'},
-    {value: 'Miami-1', viewValue: 'Miami'},
-    {value: 'Sudamérica-2', viewValue: 'Sudamérica'},
-  ];
+  champions: Datos[] = [];
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private _racesService: RacesService
+    ) {
     this.formRaces = this.fb.group ({
       name: ['', Validators.required],
-      rules: ['', Validators.required],
+      track: ['', Validators.required],
       startTime: ['', Validators.required],
       endTime: ['', Validators.required],
       startDate: ['', Validators.required],
       endDate: ['', Validators.required],
-      budget: ['', Validators.required]
+      country: ['', Validators.required],
+      champion: ['', Validators.required]
       })
    }
 
   ngOnInit(): void {
+    this.getChampions()
+  }
+
+  public getChampions() {
+    this._racesService.getChampions().subscribe(
+      result => {
+        var counter=0;
+        
+        while(result[counter]!= undefined){
+          var champion={value:result[counter].iD_tournament, viewValue:result[counter].tName}
+          console.log(result[counter].tName);
+          this.champions.push(champion)
+          console.log(champion);
+          counter++;
+        }
+      }
+    )
+  }
+
+  public SaveRace() {
+    console.log("Save Values");
+    console.log(this.formRaces.value);
   }
 
 }
--- XF1-Fantasy-Ui/src/app/services/races.service.ts
+++ XF1-Fantasy-Ui/src/app/services/races.service.ts
@@ -12,6 +12,17 @@ export class RacesService {
   constructor(
     public _http: HttpClient
   ) {
-      this.url =  "https://nutritecapi.azurewebsites.net/";
+      this.url =  "https://apixfia.azurewebsites.net/";
     }
+
+  public getChampions(): Observable<any>{
+    return this._http.get(this.url + 'api/tournament');
+  }
+
+  public createRace(raceData:Object) {
+    console.log(this.url+'api/race/newrace');
+      this._http.post(this.url+'api/race/newrace', raceData).subscribe(data => {
+      console.log(data);
+    });
+  }
 }
