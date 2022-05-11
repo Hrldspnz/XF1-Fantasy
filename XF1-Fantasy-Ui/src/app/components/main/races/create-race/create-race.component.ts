541edb4690b7c2a18e5f23a9daff322eb8f81ed9
FALCH2000 <faleiva.ch@gmail.com>
2022-05-11 09:15:08 -0600

    Updates in Ui

--- XF1-Fantasy-Ui/src/app/components/main/races/create-race/create-race.component.ts
+++ XF1-Fantasy-Ui/src/app/components/main/races/create-race/create-race.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-interface Pais {
+interface Datos {
   value: string;
   viewValue: string;
 }
@@ -15,10 +15,16 @@ export class CreateRaceComponent implements OnInit {
 
   formRaces: FormGroup;
 
-  paises: Pais[] = [
+  paises: Datos[] = [
     {value: 'Costa Rica-0', viewValue: 'Costa Rica'},
     {value: 'Panama-1', viewValue: 'Panama'},
     {value: 'Colombia-2', viewValue: 'Colombia'},
+  ];
+
+  campeonatos: Datos[] = [
+    {value: 'Costa Rica-0', viewValue: 'Costa Rica'},
+    {value: 'Miami-1', viewValue: 'Miami'},
+    {value: 'Sudamérica-2', viewValue: 'Sudamérica'},
   ];
 
   constructor(private fb: FormBuilder) {
