e09e0fafad01d846e7d00726fed3cf929981f2cc
FALCH2000 <faleiva.ch@gmail.com>
2022-05-11 09:31:00 -0600

    Addition of link to api

--- XF1-Fantasy-Ui/src/app/services/races.service.ts
+++ XF1-Fantasy-Ui/src/app/services/races.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RacesService {
 
-  constructor() { }
+  public url: string;
+
+  constructor(
+    public _http: HttpClient
+  ) {
+      this.url =  "https://nutritecapi.azurewebsites.net/";
+    }
 }
