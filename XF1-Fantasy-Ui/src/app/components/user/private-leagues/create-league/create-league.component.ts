33878f3ed3772ad533e28d7a90ce20100b4dde0e
Carmen Araya <valentinarayach64@gmail.com>
2022-06-07 08:37:39 -0600

    Updates

--- XF1-Fantasy-Ui/src/app/components/user/private-leagues/create-league/create-league.component.ts
+++ XF1-Fantasy-Ui/src/app/components/user/private-leagues/create-league/create-league.component.ts
@@ -3,6 +3,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Menu } from 'src/app/interfaces/menu';
 import { MenuService } from 'src/app/services/menu.service';
 
+interface Datos {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-create-league',
   templateUrl: './create-league.component.html',
@@ -12,6 +17,25 @@ export class CreateLeagueComponent implements OnInit {
 
   formPrivateLeague: FormGroup;
   menu: Menu[] = [];
+
+  countries: Datos[] = [
+    {value: '5-0', viewValue: '5'},
+    {value: '6-1', viewValue: '6'},
+    {value: '7-2', viewValue: '7'},
+    {value: '8-3', viewValue: '8'},
+    {value: '9-4', viewValue: '9'},
+    {value: '10-5', viewValue: '10'},
+    {value: '11-6', viewValue: '11'},
+    {value: '12-7', viewValue: '12'},
+    {value: '13-8', viewValue: '13'},
+    {value: '14-9', viewValue: '14'},
+    {value: '15-10', viewValue: '15'},
+    {value: '16-11', viewValue: '16'},
+    {value: '17-12', viewValue: '17'},
+    {value: '18-13', viewValue: '18'},
+    {value: '19-14', viewValue: '19'},
+    {value: '20-15', viewValue: '20'}
+  ];
 
   constructor(private fb: FormBuilder, private _menuService: MenuService) {
     this.formPrivateLeague = this.fb.group ({
