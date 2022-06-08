9e9d26bfc0c7f4ced667b4e1fc1b54236797af08
FALCH2000 <faleiva.ch@gmail.com>
2022-06-07 20:06:35 -0600

    update public league service

--- XF1-Fantasy-Ui/src/app/services/public-league.service.ts
+++ XF1-Fantasy-Ui/src/app/services/public-league.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PublicLeagueService {
 
-  constructor() { }
+  url:string;
+
+  constructor(
+    public _http: HttpClient 
+  ) { 
+    this.url = "https://apixfia.azurewebsites.net/"
+  }
+
+  getParticipants():Observable<any>{
+    return this._http.get(this.url+'api/league/publiclparticipants')
+  }
+
 }
