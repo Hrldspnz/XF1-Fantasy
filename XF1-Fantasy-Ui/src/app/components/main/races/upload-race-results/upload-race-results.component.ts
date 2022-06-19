81277d6454893be474e6c6427d3f80e42b73ad08
FALCH2000 <faleiva.ch@gmail.com>
2022-06-18 23:25:53 -0600

    add table

--- XF1-Fantasy-Ui/src/app/components/main/races/upload-race-results/upload-race-results.component.ts
+++ XF1-Fantasy-Ui/src/app/components/main/races/upload-race-results/upload-race-results.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { RacesService } from 'src/app/services/races.service';
+
+
+interface Positions {
+  nameUser: string;
+  country: string;
+  score:number;
+  pos:string;
+  email:string;
+}
 
 @Component({
   selector: 'app-upload-race-results',
@@ -7,9 +17,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadRaceResultsComponent implements OnInit {
 
-  constructor() { }
+  // Atributes
+  displayedColumns = ['pos','nameUser','nameTeam', 'country', 'score','select'];
+  data: Positions[] = [{
+  nameUser: 'Hola',
+  country: 'Alajuela',
+  score:12,
+  pos:'1',
+  email:'jasas'
+},
+{
+  nameUser: 'jesus',
+  country: 'nicaragua',
+  score:10,
+  pos:'2',
+  email:'jasrses'
+}];
+
+  constructor(private _racesService: RacesService) { }
 
   ngOnInit(): void {
+  }
+
+  uploadRaces(){
+    this._racesService.getRaces().subscribe(
+      result=>{
+
+      }
+    )
   }
 
 }
