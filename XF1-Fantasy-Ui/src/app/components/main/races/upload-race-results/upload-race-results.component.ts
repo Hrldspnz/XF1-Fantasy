1e108f18d05945659fb054e5904046033268abcc
FALCH2000 <faleiva.ch@gmail.com>
2022-06-18 21:26:59 -0600

    Component creation

---
+++ XF1-Fantasy-Ui/src/app/components/main/races/upload-race-results/upload-race-results.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-race-results',
+  templateUrl: './upload-race-results.component.html',
+  styleUrls: ['./upload-race-results.component.css']
+})
+export class UploadRaceResultsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
